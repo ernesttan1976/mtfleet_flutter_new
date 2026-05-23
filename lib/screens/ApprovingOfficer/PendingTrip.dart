be9d9445a41017d558f22f9f9189f778de3eab82
Ernest Tan <ernesttan1976@gmail.com>
2026-05-23 10:52:30 +0800

    Refactor PendingTripScreen for improved code clarity and consistency
    - Updated import alias for request to follow naming conventions.
    - Changed constructor and state class declaration to be const for performance optimization and clarity.
    - Enhanced error handling by providing default messages for unknown errors.
    - Simplified widget structures and improved padding declarations for better maintainability.

--- lib/screens/ApprovingOfficer/PendingTrip.dart
+++ lib/screens/ApprovingOfficer/PendingTrip.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:transport_flutter/components/AlertDialog.dart';
@@ -8,27 +7,27 @@ import 'package:transport_flutter/components/ApprovingOfficer/PendingTripCard.da
 import 'package:transport_flutter/components/Empty.dart';
 import 'package:transport_flutter/models/models.dart';
 import 'package:transport_flutter/util/currentUserData.dart';
-import 'package:transport_flutter/util/request.dart' as Request;
+import 'package:transport_flutter/util/request.dart' as request_util;
 
 import 'trip_approval_one.dart';
 
 class PendingTripsScreen extends StatefulWidget {
-  PendingTripsScreen({Key? key}) : super(key: key);
+  const PendingTripsScreen({Key? key}) : super(key: key);
 
   @override
-  _PendingTripsScreenState createState() => _PendingTripsScreenState();
+  PendingTripsScreenState createState() => PendingTripsScreenState();
 }
 
-class _PendingTripsScreenState extends State<PendingTripsScreen> {
+class PendingTripsScreenState extends State<PendingTripsScreen> {
   String? userID;
   bool _isLoading = false;
-  var request = new Request.Request();
+  final request = request_util.Request();
   final _myTrips = BehaviorSubject<List<TripDriverModel>>();
 
   @override
   void initState() {
     super.initState();
-    this.loadCurrentUser();
+    loadCurrentUser();
     _fetchListMyTrip();
   }
 
@@ -42,10 +41,11 @@ class _PendingTripsScreenState extends State<PendingTripsScreen> {
     final authString = await getUser();
     final auth = jsonDecode(authString);
     final user = auth['user']['id'];
-    if (this.mounted)
+    if (mounted) {
       setState(() {
         userID = "$user";
       });
+    }
   }
 
   void _fetchListMyTrip() async {
@@ -55,21 +55,21 @@ class _PendingTripsScreenState extends State<PendingTripsScreen> {
     try {
       final res = await request.get(Uri.parse('trips/approving-officer?approvalStatus=Pending'));
       if (res.statusCode == 200 || res.statusCode == 201) {
-        final _list = (json.decode(res.body) as List).map((e) {
+        final trips = (json.decode(res.body) as List).map((e) {
           print(e);
           return TripDriverModel.fromJson(e);
         }).toList();
-        _list.sort((a, b) => b.id.compareTo(a.id));
-        _myTrips.add(_list);
+        trips.sort((a, b) => b.id.compareTo(a.id));
+        _myTrips.add(trips);
       } else {
         if (res.statusCode == 401) {
           await storage.deleteAll();
           Navigator.pushReplacementNamed(context, '/login');
         }
-        showAlertDialog(context, 'Error', res.reasonPhrase, isPop: false);
+        showAlertDialog(context, 'Error', res.reasonPhrase ?? 'Unknown error', isPop: false);
       }
     } catch (e) {
-      showAlertDialog(context, 'Error', e as String, isPop: false);
+      showAlertDialog(context, 'Error', e.toString(), isPop: false);
     }
     setState(() {
       _isLoading = false;
@@ -77,45 +77,29 @@ class _PendingTripsScreenState extends State<PendingTripsScreen> {
   }
 
   List<Widget> _buildList(List<TripDriverModel> trips) {
-    List<Widget> listItems = [];
+    const padding = EdgeInsets.all(10.0);
+    final listItems = <Widget>[];
 
-    for (var item in trips) {
+    for (final item in trips) {
       listItems.add(Padding(
-          padding: new EdgeInsets.all(10.0),
+          padding: padding,
           child: Card(
             clipBehavior: Clip.antiAlias,
             elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
             ),
             child: InkWell(
               onTap: () async {
                 await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => TripApprovalScreen(
-                        tripID: item.id.toString(),
-                      ),
-                    ));
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TripApprovalScreen(
+                      tripID: item.id.toString(),
+                    ),
+                  ),
+                );
                 _fetchListMyTrip();
-                // if (item['mt_rac_form'] == null) {
-                //   await Navigator.push(
-                //       context,
-                //       MaterialPageRoute(
-                //         builder: (context) => TripApprovalScreen(
-                //           tripID: item.id,
-                //         ),
-                //       ));
-                //   refetch();
-                // } else {
-                //   await Navigator.push(
-                //       context,
-                //       MaterialPageRoute(
-                //         builder: (context) => MTRACApprovalFirstScreen(
-                //           tripID: item['id'],
-                //         ),
-                //       ));
-                // }
               },
               child: PendingTripCard(tripData: item),
             ),
@@ -131,23 +115,28 @@ class _PendingTripsScreenState extends State<PendingTripsScreen> {
         ? Stack(
             children: <Widget>[
               StreamBuilder<List<TripDriverModel>>(
-                  stream: _myTrips,
-                  initialData: [],
-                  builder: (context, snapshot) {
-                    if (snapshot.data!.isEmpty) {
-                      return Center(
-                        child: EmptyPlaceholder(
-                            description: "Pending Trips not found.", imagePath: "assets/images/no_data.png"),
-                      );
-                    }
-                    return ListView(
-                      children: _buildList(snapshot.data!),
+                stream: _myTrips,
+                initialData: const [],
+                builder: (context, snapshot) {
+                  final trips = snapshot.data ?? [];
+
+                  if (trips.isEmpty) {
+                    return const Center(
+                      child: EmptyPlaceholder(
+                        description: "Pending Trips not found.",
+                        imagePath: "assets/images/no_data.png",
+                      ),
                     );
-                  }),
+                  }
+                  return ListView(
+                    children: _buildList(trips),
+                  );
+                },
+              ),
               if (_isLoading)
                 const Center(
                   child: CircularProgressIndicator(),
-                )
+                ),
             ],
           )
         : const Center(
