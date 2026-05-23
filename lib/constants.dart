714ae5a0601a5e687c2513e0268fea7b8b2027cf
Ernest Tan <ernesttan1976@gmail.com>
2026-05-23 18:54:42 +0800

    Update constants in lib/constants.dart for improved clarity and consistency
    - Added SERVER_URI and SERVER_URI_API constants for better API management.
    - Introduced AUTH_CALLBACK constant for authentication success URL.
    - Defined CURRENT_VERSION constant for version tracking.
    - Included ignore directives for constant identifier names to adhere to linting rules.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,16 @@
 library constants;
 
+// ignore: constant_identifier_names
 const String SERVER_URI = "https://v2.api.transport-uat.swiftoffice.org";
 
 //"https://v2.api.transport-uat.swiftoffice.org";
 //https://api.transport-uat.swiftoffice.org  https://api.transport.swiftoffice.org
+// ignore: constant_identifier_names
 const String SERVER_URI_API = SERVER_URI;
+// ignore: constant_identifier_names
 const String AUTH_CALLBACK = "$SERVER_URI/auth/microsoft/success";
 
+// ignore: constant_identifier_names
 const String CURRENT_VERSION = "1.4.6";
 
 const String storageBearer = "api_bearer";
