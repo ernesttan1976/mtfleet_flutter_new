96d57363621c1a92a5ebe2490a080c9234e8eb12
Ernest Tan <ernesttan1976@gmail.com>
2026-05-23 19:44:59 +0800

    Refactor LoginWebView for improved code clarity and consistency
    - Removed unused imports to streamline the code.
    - Updated variable names and error handling for better readability.
    - Enhanced role checking logic and navigation flow for improved user experience.

--- lib/screens/loginWebView.dart
+++ lib/screens/loginWebView.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:dio/dio.dart' as dio_client;
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -14,8 +13,6 @@ import 'package:transport_flutter/screens/login.dart';
 import 'package:transport_flutter/util/currentUserData.dart';
 import 'package:transport_flutter/util/request.dart' as request;
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
 class LoginAuthScreen extends StatefulWidget {
   const LoginAuthScreen({Key? key}) : super(key: key);
@@ -26,20 +23,17 @@ class LoginAuthScreen extends StatefulWidget {
 
 class _LoginAuthScreenState extends State<LoginAuthScreen> {
   final _storage = FlutterSecureStorage();
-  final dio = dio_client.Dio();
   final logger = Logger();
 
   @override
   void initState() {
     super.initState();
-    //demo_transport3@swiftoffice.org
-    //b2jr2ngb2l!
 
-    // Enable virtual display.
+    // Enable virtual display for Android.
     if (Platform.isAndroid) {
-      final PlatformWebViewControllerCreationParams params =
-          AndroidWebViewControllerCreationParams();
-      final WebViewController controller =
+      PlatformWebViewControllerCreationParams params =
+          const PlatformWebViewControllerCreationParams();
+      WebViewController controller =
           WebViewController.fromPlatformCreationParams(params);
       WebViewWidget(controller: controller);
     }
@@ -50,7 +44,7 @@ class _LoginAuthScreenState extends State<LoginAuthScreen> {
     String loginUrl = '${constants.SERVER_URI_API}/auth/microsoft/login';
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sign In Using Microsoft"),
+        title: const Text('Sign In Using Microsoft'),
       ),
       body: WebViewWidget(
         controller: WebViewController()
@@ -59,59 +53,73 @@ class _LoginAuthScreenState extends State<LoginAuthScreen> {
           ..setNavigationDelegate(
             NavigationDelegate(
               onPageFinished: (url) async {
-                if (mounted) {
-                  if (url.startsWith(constants.AUTH_CALLBACK)) {
-                    try {
-                      var token = url.split("access_token=")[1];
-                      await _storage.write(key: constants.storageBearer, value: token);
-                      var httpRequest = request.Request();
-                      var data = json.decode((await httpRequest.get(Uri.parse("users/me"))).body);
-                      logger.e("loginData-> $data");
-                      final roles = <String>[];
-                      for (final role in (data['roles'] as List)) {
-                        if (supportedRoles.contains(role)) {
-                          roles.add(role);
-                        }
-                      }
-                      if (roles.isNotEmpty) {
-                        var currentRole = roles.first;
-                        var authData = {
-                          "user": {...data, "otherRoles": []}
-                        };
-                        await _storage.write(key: "auth", value: json.encode(authData));
-                        await _storage.write(key: "currentRole", value: currentRole);
-                        if (roles.contains("APPROVING_OFFICER")) {
-                          _setFirebaseToken(data);
-                        } else {
-                          _deleteFirebaseToken(data);
-                        }
+                if (!mounted) return;
 
-                        Navigator.pushReplacementNamed(context, '/appLoading');
-                      } else {
-                        Navigator.of(context).popUntil((route) => route.isFirst);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LoginScreen(
-                              error:
-                                  "You don't have a proper role to access the application. Please contact the administration.",
-                            ),
-                          ),
-                        );
+                if (url.startsWith(constants.AUTH_CALLBACK)) {
+                  try {
+                    final token = url.split('access_token=')[1];
+                    await _storage.write(key: constants.storageBearer, value: token);
+
+                    final httpRequest = request.Request();
+                    final response = await httpRequest.get(Uri.parse('users/me'));
+                    final data = json.decode(response.body) as Map<String, dynamic>;
+
+                    logger.e('loginData-> $data');
+
+                    final roles = <String>[];
+                    for (final role in (data['roles'] as List)) {
+                      if (supportedRoles.contains(role)) {
+                        roles.add(role as String);
                       }
-                    } catch (e, trace) {
-                      logger.e("Error In Login $e $trace");
-                      await Sentry.captureException(e, stackTrace: trace);
+                    }
+
+                    if (roles.isNotEmpty) {
+                      final currentRole = roles.first;
+                      final authData = {
+                        'user': {...data, 'otherRoles': []},
+                      };
+
+                      await _storage.write(key: 'auth', value: json.encode(authData));
+                      await _storage.write(key: 'currentRole', value: currentRole);
+
+                      if (roles.contains('APPROVING_OFFICER')) {
+                        _setFirebaseToken(data);
+                      } else {
+                        _deleteFirebaseToken(data);
+                      }
+
+                      if (!mounted) return;
+                      Navigator.pushReplacementNamed(context, '/appLoading');
+                    } else {
+                      if (!mounted) return;
+
                       Navigator.of(context).popUntil((route) => route.isFirst);
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => LoginScreen(
-                            error: "There was some internal error while logging you in. Please try again!",
+                          builder: (context) => const LoginScreen(
+                            error:
+                                "You don't have a proper role to access the application. Please contact the administration.",
                           ),
                         ),
                       );
                     }
+                  } catch (e, trace) {
+                    logger.e('Error In Login $e $trace');
+                    await Sentry.captureException(e, stackTrace: trace);
+
+                    if (!mounted) return;
+
+                    Navigator.of(context).popUntil((route) => route.isFirst);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginScreen(
+                          error:
+                              'There was some internal error while logging you in. Please try again!',
+                        ),
+                      ),
+                    );
                   }
                 }
               },
@@ -121,46 +129,53 @@ class _LoginAuthScreenState extends State<LoginAuthScreen> {
     );
   }
 
-  void _setFirebaseToken(dynamic data) async {
+  Future<void> _setFirebaseToken(dynamic data) async {
     try {
       final client = AuthedDio.instance.dio;
       final dioClient = await client;
-      FirebaseMessaging messaging = FirebaseMessaging.instance;
-      try {
-        messaging.subscribeToTopic("User-${data["id"]}");
-      } catch (e) {
-        logger.e("SubScribeToTopic $e");
-      }
-      final token = await messaging.getToken();
-      var dataJSON = jsonEncode({"token": token});
-      var response = await dioClient.post("/users/me/fcm/subscribe", data: dataJSON);
-      logger.e("Firebase Token $response");
+      final FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-      if (response.statusCode == 201) {
-      } else {
-        showAlertDialog(context, "Error", response.data['message'], isPop: false);
+      try {
+        await messaging.subscribeToTopic('User-${data['id']}');
+      } catch (e) {
+        logger.e('SubScribeToTopic $e');
+      }
+
+      final token = await messaging.getToken();
+      final dataJSON = jsonEncode({'token': token});
+      final response = await dioClient.post('/users/me/fcm/subscribe', data: dataJSON);
+      logger.e('Firebase Token $response');
+
+      if (response.statusCode != 201 && mounted) {
+        showAlertDialog(context, 'Error', response.data['message'], isPop: false);
       }
     } catch (e) {
-      showAlertDialog(context, "Error", e.toString());
+      if (mounted) {
+        showAlertDialog(context, 'Error', e.toString());
+      }
     }
   }
 
-  void _deleteFirebaseToken(dynamic data) async {
+  Future<void> _deleteFirebaseToken(dynamic data) async {
     try {
       final client = AuthedDio.instance.dio;
       final dioClient = await client;
-      FirebaseMessaging messaging = FirebaseMessaging.instance;
+      final FirebaseMessaging messaging = FirebaseMessaging.instance;
+
       try {
-        messaging.unsubscribeFromTopic("User-${data["id"]}");
+        await messaging.unsubscribeFromTopic('User-${data['id']}');
       } catch (e) {
-        logger.e("unsubscribeFromTopic $e");
+        logger.e('unsubscribeFromTopic $e');
       }
+
       final token = await messaging.getToken();
-      var dataJSON = jsonEncode({"token": token});
-      await dioClient.post("/users/me/fcm/unsubscribe", data: dataJSON);
+      final dataJSON = jsonEncode({'token': token});
+      await dioClient.post('/users/me/fcm/unsubscribe', data: dataJSON);
       await messaging.deleteToken();
     } catch (e) {
-      showAlertDialog(context, "Error", e.toString(), isPop: false);
+      if (mounted) {
+        showAlertDialog(context, 'Error', e.toString(), isPop: false);
+      }
     }
   }
 }
